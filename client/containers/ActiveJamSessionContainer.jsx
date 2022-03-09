0da84667574962dcf2996e050d25f693d639816a
HakudoUeno <hakudo.0909@gmail.com>
2022-03-09 17:27:57 -0500

    add search bar not connected to spotify api

--- client/containers/ActiveJamSessionContainer.jsx
+++ client/containers/ActiveJamSessionContainer.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
+import SearchBar from '../components/SearchBar.jsx';
+import SearchResult from '../components/SearchResult.jsx';
 
 
 const ActiveJamSessionContainer = (props) => {
   return (
     <div>
         <h1>{props.currentJamSession}</h1>
+        <SearchBar/>
         <button className="button is-light" onClick={()=>{props.deactivateJamSession()}}>DEACTIVATE</button>
     </div>  
   )
