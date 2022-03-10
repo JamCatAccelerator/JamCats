9cf28737617d3605b82a7ae81f95b3441f5aeb57
Aiden Blinn <apblinn@gmail.com>
2022-03-10 10:22:28 -0600

    add song / search functionality

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import LoginContainer from './LoginContainer.jsx';
 import HomeContainer from './HomeContainer.jsx';
 
 function MainContainer() {
   let [isAuthenticated, setAuthenticationStatus] = useState(false);
-  let [userObj, setUserObj] = useState([{ 
-    spotifyProfile:'spotifyProfile', 
-    dbInfo:'dbInfo', 
-    authenticated:'authenticated', 
-    jamSessions:'jamSessions', 
-    playlists:'playlists'
-  }]); //{ spotifyProfile, dbInfo, authenticated, jamSessions, playlists }
+  let [userObj, setUserObj] = useState([
+    {
+      spotifyProfile: 'spotifyProfile',
+      dbInfo: 'dbInfo',
+      authenticated: 'authenticated',
+      jamSessions: 'jamSessions',
+      playlists: 'playlists',
+    },
+  ]); //{ spotifyProfile, dbInfo, authenticated, jamSessions, playlists }
 
-  useEffect ( () => {
+  useEffect(() => {
     // let authStatus = confirm('would you like to log in')
     // setAuthenticationStatus(authStatus);
     fetch('http://localhost:8080/user/info')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      // setAuthenticationStatus(true);
-      // setUserObj(data);
-    })
-    .catch((error) => {console.error('Error useEffect:', error);})
-  },[])
-  
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setAuthenticationStatus(true);
+        // setUserObj(data);
+      })
+      .catch((error) => {
+        console.error('Error useEffect:', error);
+      });
+  }, []);
 
-  if (!isAuthenticated){
-    console.log('SHOULD BE FALSE-> ', isAuthenticated)
+  if (!isAuthenticated) {
+    console.log('SHOULD BE FALSE-> ', isAuthenticated);
     return (
       <div>
         <div>
-          <Navbar isAuthenticated={isAuthenticated}/>
-          <LoginContainer/>
+          <Navbar isAuthenticated={isAuthenticated} />
+          <LoginContainer />
         </div>
       </div>
-    )
-  }else{
-    console.log('SHOULD BE TRUE -> ', isAuthenticated)
-    return(
-    <div>
-        <div><Navbar isAuthenticated={isAuthenticated}/></div>
-        <HomeContainer userObj = {userObj}
-        />
-    </div>
+    );
+  } else {
+    console.log('SHOULD BE TRUE -> ', isAuthenticated);
+    return (
+      <div>
+        <div>
+          <Navbar isAuthenticated={isAuthenticated} />
+        </div>
+        <HomeContainer userObj={userObj} />
+      </div>
     );
   }
 }
